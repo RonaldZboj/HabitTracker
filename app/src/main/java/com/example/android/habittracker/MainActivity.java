e03804d5e458aedb36d4464872741b5e598ccb44
RonaldZboj <ronaldzboj@gmail.com>
2017-07-03 20:24:16 +0200

    added MainActivity and declare it in the AndroidManifest as the Main Activity to  meet the requirements

---
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.habittracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RonaldZboj on 2017-07-03.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
